d7aae5e72b483f9d2e5d85a2c73793bcafbb7ee9
Ian Callahan <ian.callahan@gmail.com>
2019-03-26 00:05:18 -0400

    Adding explanatory text to home and Readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # solve_challenge
+
+Hello!
+
+Here are some important things to know about this demo application:
+- JS and CSS assets are compiled with Laravel Mix (`npm run production`)
+- The profile editor includes a Vue.js component that auto-saves biography edits
+- The production instance of this demo is loaded with Mailgun credentials so the new user email feature can be tested.
+
+Thanks for taking a look at it.
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Home</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You have successfully logged in to this demo application. If you're a regular user, you can edit your <a href="{{route('profile.edit')}}">profile</a>. If you're an admin, you can also <a href="{{route('admin.users.index')}}">create, edit and delete users.</a> 
                 </div>
             </div>
         </div>
